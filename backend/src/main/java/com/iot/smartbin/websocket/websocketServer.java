31b389dea3726a6287bef68d7164fb6e6b5121da
Daniel Deng <denjm@foxmail.com>
2023-03-31 15:49:38 +0100

    fix websocket problem

--- backend/src/main/java/com/iot/smartbin/websocket/websocketServer.java
+++ backend/src/main/java/com/iot/smartbin/websocket/websocketServer.java
@@ -13,7 +13,6 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-
 @Component
 @ServerEndpoint("/websocket/client")
 @Slf4j
@@ -21,18 +20,18 @@ public class websocketServer {
     private Session session;
     private int onlineCount = 0;
     private static CopyOnWriteArraySet<websocketServer> webSocketSet = new CopyOnWriteArraySet<websocketServer>();
-
     @OnOpen
     public void onOpen(Session session){
-        MqttPubSubService mqttPubSubService = new MqttPubSubService();
         //TODO get topicName
+        MqttPubSubService mqttPubSubService = new MqttPubSubService();
         AWSIotTopic topic_height = new TrashHeightTopic("trash_height", AWSIotQos.QOS0, this);
         mqttPubSubService.subscribeMessage(topic_height);
-
         log.info("trash_height");
+
         AWSIotTopic topic_humidity = new TrashHeightTopic("trash_humidity", AWSIotQos.QOS0, this);
         mqttPubSubService.subscribeMessage(topic_humidity);
         log.info("trash_humidity");
+
         this.session = session;
         webSocketSet.add(this);
         onlineCount++;
