5f9979e4a99b4a4f188b6155dfc1eb2938a2364c
Daniel Deng <denjm@foxmail.com>
2023-04-04 11:53:44 +0100

    update

---
+++ backend/src/main/java/com/iot/smartbin/mqtt/s3/MyObject.java
@@ -0,0 +1,26 @@
+package com.iot.smartbin.mqtt.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class MyObject {
+    public String getURL() throws IOException {
+        String bucketName = "bucketName";
+        String key = "key";
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+        GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, key);
+        S3Object s3Object = s3Client.getObject(getObjectRequest);
+
+        InputStream objectContent = s3Object.getObjectContent();
+        byte[] content = objectContent.readAllBytes();
+        return Arrays.toString(content);
+    }
+
+}
