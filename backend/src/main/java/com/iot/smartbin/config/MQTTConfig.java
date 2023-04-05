1ef772972badfab773a51902077d1c4996162d3a
SiuyunYip <yzyyezhaoyuan@gmail.com>
2023-04-05 18:51:19 +0100

    delete access key

--- backend/src/main/java/com/iot/smartbin/config/MQTTConfig.java
+++ backend/src/main/java/com/iot/smartbin/config/MQTTConfig.java
@@ -14,17 +14,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class MQTTConfig {
-//    @Value("${clientEndpoint}")
-    private String clientEndpoint = "a3ravxmuj0c4e2-ats.iot.eu-west-1.amazonaws.com";
+    private String clientEndpoint = null;
 
-//    @Value("${clientId}")
-    private String clientId = "smartbin-backend";
+    private String clientId = null;
 
-//    @Value("${accessKey}")
-    private String accessKey = "AKIA26U2W4737NMDR4E5";
+    private String accessKey = null;
 
-//    @Value("${secretAccessKey}")
-    private String secretAccessKey = "Hg+B17edOO9X8qIEsrYbZK46uih5gBfth8TNOAxv";
+    private String secretAccessKey = null;
 
     private AWSIotMqttClient client;
 
